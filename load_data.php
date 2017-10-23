39274eb1267e6bdc40be96fa6bb3221dcad0cd65
amr yousry <amryousry92@gmail.com>
2017-10-23 21:12:00 +0200

    separating files

---
+++ load_data.php
@@ -0,0 +1,9 @@
+<?php
+// Objective #2
+
+$mysqli = new MySQLi("localhost", "root", "123", "phptest");
+$list=mysql_query("SELECT * FROM mtcproducts ORDER BY product_variant_position ASC");
+$product_name = $list.first["product_name"];
+$product_id=$list.first["product_id"];
+
+?>
