f04d15b0f4c51ee4858a1d152de8cd6e53fccec8
Amr Yousry <amr.yousry-mohamed@vodafone.com>
2017-10-24 19:59:37 +0200

    adding javascript

--- load_data.php
+++ load_data.php
@@ -6,4 +6,5 @@ $list=mysql_query("SELECT * FROM mtcproducts ORDER BY product_variant_position A
 $product_name = $list.first["product_name"];
 $product_id=$list.first["product_id"];
 
+echo json_encode(array("data"=> mysql_fetch_array($list));
 ?>
